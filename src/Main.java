ef0819075298f0bc50419052587b3ddde226179f
teshnizi2 <mohammadrezaahmaditeshnizi@gamil.com>
2020-03-15 18:52:04 +0330

    add getting input

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,11 @@
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	Scanner scanner = new Scanner(System.in);
+	String input = scanner.nextLine();
+	System.out.println(input);
+
     }
 }
